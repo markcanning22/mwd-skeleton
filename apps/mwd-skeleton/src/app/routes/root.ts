e8b7cd2d7a93938b4a03c0c73f84552e8384c34c
Mark Canning <markcanning22@users.noreply.github.com>
2025-07-14 21:22:29 +0100

    wip - created a shared lib that is responsible for saying 'Hello API and hooked into app'

--- apps/mwd-skeleton/src/app/routes/root.ts
+++ apps/mwd-skeleton/src/app/routes/root.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify';
+import { shared } from '@mwd-skeleton/shared';
 
 export default async function (fastify: FastifyInstance) {
   fastify.get('/', async function () {
-    return { message: 'Hello API' };
+    return { message: shared() };
   });
 }
